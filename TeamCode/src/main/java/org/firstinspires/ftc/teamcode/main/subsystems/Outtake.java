49e7963b81fcf10a61b5d908ede7727c94f9763b
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2023-05-12 21:41:36 -0700

    Change the safe autos to take a safer path (TODO: Test multiple times for reliability)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/subsystems/Outtake.java
@@ -46,8 +46,8 @@ public class Outtake {
 
     private static double LATCH_MIN = 0.28; // Guess (not that it matters)
     private static double LATCH_MAX = 0.85; // Guess
-    public static double LATCH_OPEN = 0.32;
-    public static double LATCH_BARELY = 0.73; // Just covers the bottom, does not apply pressure
+    public static double LATCH_OPEN = 0.25;
+    public static double LATCH_BARELY = 0.59; // Just covers the bottom, does not apply pressure
     public static double LATCH_ENGAGED = 0.98; // may disable the servo temporarily
 
     private static double GUIDE_MIN = 0.17;
